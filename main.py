bf4fa16a6905ef238ad48dfe46083369b0acff50
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-05-03 15:19:12 +0200

    Change data directory to dist

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ res = {}
 items = ["taux_incidence","hospitalisations","soins_critiques","deces","vaccins","cas_positifs","taux_positivite"]
 
 res['taux_incidence'] = getTauxIncidence()
-''' es['hospitalisations'] = getHospitalisations()
+res['hospitalisations'] = getHospitalisations()
 res['hospitalisations_moyenne_quotidien'] = getMeanHospitalisations()
 res['soins_critiques'] = getReas()
 res['soins_critiques_moyenne_quotidien'] = getMeanReas()
@@ -17,20 +17,20 @@ res['deces'] = getDeces()
 res['deces_moyenne_quotidien'] = getMeanDeces()
 res['vaccins_premiere_dose'] = getFirstDoseVaccins()
 res['vaccins_vaccines'] = getFullVaccins()
-res['cas_positifs'] = getCasPositifs() '''
+res['cas_positifs'] = getCasPositifs()
 res['taux_positivite'] = getTauxPositivite()
 
 for item in res:
-    with open('data/'+item+'.json','w') as fp:
+    with open('dist/'+item+'.json','w') as fp:
         json.dump(res[item], fp)
 
 resglobal = {}
-files = glob.glob("data/*.json")
+files = glob.glob("dist/*.json")
 
 for file in files:
     with open(file) as json_file:
-        print(file.replace('data/','').replace('.json',''))
-        resglobal[file.replace('data/','').replace('.json','')] = json.load(json_file)
+        print(file.replace('dist/','').replace('.json',''))
+        resglobal[file.replace('dist/','').replace('.json','')] = json.load(json_file)
 
 with open('global_new.json','w') as fp:
     json.dump(resglobal, fp)
