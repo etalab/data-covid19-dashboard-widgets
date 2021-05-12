6689ae3406f1af3cda10c6055cca5d0d814eda9c
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-05-12 16:54:40 +0200

    Remove Raise Exception

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from utils import get_taux, get_kpi, get_kpi_only_france, get_taux_specific
 from download_and_check_files import download_and_check, save_new_files, shorten_and_save
 from logger import log
 
-raise Exception()
-
 kpis = download_and_check()
 
-#kpis = ['hospitalisations', 'hospitalisations_moyenne_quotidienne', 'retour_a_domicile', 'retour_a_domicile_moyenne_quotidienne', 'soins_critiques', 'soins_critiques_moyenne_quotidienne', 'deces', 'deces_moyenne_quotidienne', 'cas_positifs', 'taux_incidence', 'taux_positivite', 'vaccins_premiere_dose', 'vaccins_premiere_dose_moyenne_quotidienne', 'vaccins_vaccines','vaccins_vaccines_moyenne_quotidienne','taux_occupation','facteur_reproduction']
+# kpis = ['hospitalisations', 'hospitalisations_moyenne_quotidienne', 'retour_a_domicile', 'retour_a_domicile_moyenne_quotidienne', 'soins_critiques', 'soins_critiques_moyenne_quotidienne', 'deces', 'deces_moyenne_quotidienne', 'cas_positifs', 'taux_incidence', 'taux_positivite', 'vaccins_premiere_dose', 'vaccins_premiere_dose_moyenne_quotidienne', 'vaccins_vaccines','vaccins_vaccines_moyenne_quotidienne','taux_occupation','facteur_reproduction']
 
 log.debug(kpis)
 
